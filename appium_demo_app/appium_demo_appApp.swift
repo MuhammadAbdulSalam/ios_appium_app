5c90b9f2454327f2a5d217051a74a54028563bf4
Muhammad AbdulSalam <muhammadabdulsalam@Muhammads-MacBook-Pro.local>
2022-12-05 14:55:03 +0000

    initial commit

---
+++ appium_demo_app/appium_demo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appium_demo_appApp.swift
+//  appium_demo_app
+//
+//  Created by Muhammad AbdulSalam on 05/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct appium_demo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
